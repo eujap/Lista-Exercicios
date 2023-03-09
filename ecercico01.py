1ca514a0b0480694e9b218d0cdbb120ebdb3d8b3
DESKTOP-BV2KHFU\ACADEMICO <diego@katecnolog1a.com.br>
2023-03-08 21:55:14 -0400

    inicio exercicio de python

---
+++ ecercico01.py
@@ -0,0 +1,13 @@
+def cadastros(nome, horas_trabalhadas_mes, turno_de_trabalho, ocupacao):
+    While(
+    nome = input("Nome do colaborador: ")
+    horas_tabalhadas_mes = input("Numeros de Horas Trabalhadas: ")
+    turno_de_trabalho = input("Informe M = Matutino, V = Vespertino e N = Noturno : ")
+    ocupacao = input("Informe ocupação (G = Gerente e O = Operário):  ")
+    else: imput
+
+    )
+
+colaboradores = [nome, horas_tabalhadas_mes, turno_de_trabalho, ocupacao  ]
+
+print(colaboradores)
