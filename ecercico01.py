a9b29b773b616e0ea828243635c130ec9169b4ec
DESKTOP-BV2KHFU\ACADEMICO <diego@katecnolog1a.com.br>
2023-03-13 21:56:26 -0400

    aula dia 13.03

--- ecercico01.py
+++ ecercico01.py
@@ -1,14 +1,32 @@
-def cadastros(nome, horas_trabalhadas_mes, turno_de_trabalho, ocupacao):
 
-    while (cadastros == s):
-        nome = input("Nome do colaborador: ")
+def cadastros():
+    nome=''
+    horas_tabalhadas_mes=0
+    turno_de_trabalho=0
+    ocupacao=''
+
+    while True:
+        nome = input(" Nome do colaborador: ")
         horas_tabalhadas_mes = input("Numeros de Horas Trabalhadas: ")
         turno_de_trabalho = input("Informe M = Matutino, V = Vespertino e N = Noturno : ")
         ocupacao = input("Informe ocupação (G = Gerente e O = Operário):  ")
 
-        continua = input("deseja continuar? ")
-    
+        continua = input("deseja continuar? 1-Sim e 2-Não")
+        if continua  == "2":
+            continua = ''
+            return False
+  
+#    print(colaboradores)
 
-colaboradores = [nome, horas_tabalhadas_mes, turno_de_trabalho, ocupacao  ]
+#def valor_hora_trabalhada(ocupacao, turno_de_trabalho, valor_hora_trabalhada ):
+#    valor_hora_trabalhada = float () 
 
-print(colaboradores)
+def calcular_salario():
+    horas_tabalhadas_mes = float(input("Numeros de Horas Trabalhadas: "))
+    valor_hora_trabalhada = float(input("valor_hora_trabalhada"))
+    if horas_tabalhadas_mes:
+        salario=horas_tabalhadas_mes*valor_hora_trabalhada
+    return salario
+
+cadastros()  
+calcular_salario()
