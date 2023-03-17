dd0dfd1de9c7665ac7b49a052015ba2421b8abe2
eujap <jose.ap2008@hotmail.com>
2023-03-17 17:03:55 -0400

    adequado a laço de repetição e incluindo os verificadores no exercicio 2, criando lista do exercico 1

--- ecercico01.py
+++ ecercico01.py
@@ -1,49 +1,51 @@
-def cadastros():
-    
-    nome=''
-    horas_tabalhadas_mes = 0
-    turno_de_trabalho=''
-    ocupacao=''
+cadastros = []
+temp = []
 
-    while True:
-        nome = input(" Nome do colaborador: ")
-        horas_tabalhadas_mes = input("Numeros de Horas Trabalhadas: ")
-        turno_de_trabalho = input("Informe M = Matutino, V = Vespertino e N = Noturno : ")
-        ocupacao = input("Informe ocupação (G = Gerente e O = Operário):  ")
+while True:
+    temp.append(str(input(" Nome do colaborador:  ")))
+    temp.append(float(input(" Quantidade de horas trabalhadas:  ")))
+    temp.append(str(input(" Turno de trabalho: M=Matutino, V=Vespertino e N=Noturno:  ")))
+    temp.append(str(input(" Ocupação do colaborador: Gerente = G, Operario = O:  ")))
+    cadastros.append(temp[:])
+    temp.clear()
 
-        continua = input("deseja continuar? 1-Sim e 2-Não")
-        if continua  == "2":
-            continua = ''
-            return False
-  
+    continua = input("Dejesa cadastar outro? 1= Sim ou 2 = Não")
+    if continua == "2":
+        contiua = ''
+        break
+
+print(f' os dados foram {cadastros}')    
 
 
-def valor_hora_trabalhada():
-    ocupacao = ''
-    turno_de_trabalho = ''
-    valor_base = 1_320.00
-    
-    if ocupacao == "G" and (turno_de_trabalho == "V" or "M") :
-            valor_hora_trabalhada = float(valor_base*0.15)
-    elif ocupacao == "G" and (turno_de_trabalho == "N") :      
-            valor_hora_trabalhada = (valor_base*0.10)
-    elif ocupacao == "O" and (turno_de_trabalho == "N") :
-            valor_hora_trabalhada = (valor_base*0.09)
-    elif ocupacao == "O" and (turno_de_trabalho == "V" or "M") :
-            valor_hora_trabalhada = (valor_base*0.14)
+#def valor_hora_trabalhada():
+#    ocupacao = ''
+#    turno_de_trabalho = 0
+#    valor_base = 1_320.00
+#    
+#    if ocupacao == "G" and (turno_de_trabalho == "V" or "M") :
+#            valor_hora_trabalhada = float(valor_base*0.15)
+#    elif ocupacao == "G" and (turno_de_trabalho == "N") :      
+#            valor_hora_trabalhada = (valor_base*0.10)
+#    elif ocupacao == "O" and (turno_de_trabalho == "N") :
+#            valor_hora_trabalhada = (valor_base*0.09)
+#    elif ocupacao == "O" and (turno_de_trabalho == "V" or "M") :
+#            valor_hora_trabalhada = (valor_base*0.14)
 
+#
+
+#valor_hora_trabalhada()
+#print(valor_hora_trabalhada)
  
-def calcular_salario():
-    horas_tabalhadas_mes = float(horas_tabalhadas_mes())
-    valor_hora_trabalhada = float(valor_hora_trabalhada())
-    if horas_tabalhadas_mes:
-        salario=horas_tabalhadas_mes*valor_hora_trabalhada
-    return salario
+#def calcular_salario():
+#    horas_tabalhadas_mes = float(horas_tabalhadas_mes())
+#    valor_hora_trabalhada = float(valor_hora_trabalhada())
+#    if horas_tabalhadas_mes:
+#        salario=horas_tabalhadas_mes*valor_hora_trabalhada
+#    return salario
 
-cadastros()
-print(cadastros)
-valor_hora_trabalhada()
-print(valor_hora_trabalhada())
-calcular_salario()
-print(calcular_salario())
+#cadastro()
+#valor_hora_trabalhada()
+#print(valor_hora_trabalhada())
+#calcular_salario()
+#print(calcular_salario())
 
--- exercícios02.py
+++ exercícios02.py
@@ -1,9 +1,46 @@
 elevadores = [ "A", "B", "C"]
 turnos = ["M", "V", "N"]
-respostas =[]
+elevador = []
+periodo = []
+respostas = []
 
 while (True):
-    elevador = str(input("informe o elevador mais utilizado A, B ou C")).upper()
+    elevador = str(input("informe o elevador mais utilizado A, B ou C?  ")).upper()
     while not elevador in elevadores:
         print("digitou errado")
-        elevador = str(input("informe o elevador utilizado: A, B, C")).upper()
+        elevador = str(input("informe o elevador utilizado: A, B ou  C?  ")).upper()
+        if elevador in elevadores:
+            break
+    periodo = str(input("Em qual período em que utiliza o elevador, entre (M = Matutino, V = Vespertino, N = Noturno)?  ")).upper()
+    while not periodo in turnos:
+        print("Informe opção valida")
+        periodo = str(input("Em qual período em que utiliza o elevador, entre (M = Matutino, V = Vespertino, N = Noturno)?  ")).upper()
+        if periodo in turnos:
+            break
+    respostas.append(elevador[:])
+    respostas.append(periodo[:])
+    
+    
+
+    while True:
+        continua = str(input("Deseja continuar? S-Sim e N-Não  ")).upper()[0]
+        if continua in 'SN':
+            break
+        print("ERRO! Responda apenas S ou N. ")
+    if continua == "N":
+        break
+
+def conta_elevador():
+    elevador = ''
+    periodo = ''
+    if elevador == "A" and (periodo == "M"):
+        elev_A_manha = 0
+        elev_A_manha += 1
+    elif elevador == "A" and (periodo == "V"):
+        elev_A_tarde = 0
+        elev_A_tarde = +1 
+                
+
+print(respostas)
+conta_elevador()
+print(conta_elevador)
