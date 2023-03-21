23934987e4755466fb0d8fedd275ad329bc6840c
DESKTOP-BV2KHFU\ACADEMICO <diego@katecnolog1a.com.br>
2023-03-20 21:58:50 -0400

    aula20/03

--- exercícios02.py
+++ exercícios02.py
@@ -1,26 +1,32 @@
+def validar(msg, validos):
+    valor = str(input(msg)).upper()
+    while not valor in validos:
+        print("Digitou errado")
+        valor = str(input(msg)).upper()
+
+    return valor    
+
+
+def buscarMaiorElevador(opcoes):
+    itens = [0, 0, 0]
+    for resposta in respostas:
+        for opcao in opcoes:
+            itens[opcao.index(opcao)] +=1
+   
+        
+return opcoes[itens.infr]
+
+
+
 elevadores = [ "A", "B", "C"]
 turnos = ["M", "V", "N"]
-elevador = []
-periodo = []
 respostas = []
 
 while (True):
-    elevador = str(input("informe o elevador mais utilizado A, B ou C?  ")).upper()
-    while not elevador in elevadores:
-        print("digitou errado")
-        elevador = str(input("informe o elevador utilizado: A, B ou  C?  ")).upper()
-        if elevador in elevadores:
-            break
-    periodo = str(input("Em qual período em que utiliza o elevador, entre (M = Matutino, V = Vespertino, N = Noturno)?  ")).upper()
-    while not periodo in turnos:
-        print("Informe opção valida")
-        periodo = str(input("Em qual período em que utiliza o elevador, entre (M = Matutino, V = Vespertino, N = Noturno)?  ")).upper()
-        if periodo in turnos:
-            break
-    respostas.append(elevador[:])
-    respostas.append(periodo[:])
-    
-    
+    elevador = validar("informe o elevador mais utilizado A, B ou C?  ", elevadores)
+    turnos = validar('Informe o turno mais utilizado:( M, V, N)', turnos)
+
+    respostas.append([elevador, turnos])
 
     while True:
         continua = str(input("Deseja continuar? S-Sim e N-Não  ")).upper()[0]
@@ -30,17 +36,20 @@ while (True):
     if continua == "N":
         break
 
-def conta_elevador():
-    elevador = ''
-    periodo = ''
-    if elevador == "A" and (periodo == "M"):
-        elev_A_manha = 0
-        elev_A_manha += 1
-    elif elevador == "A" and (periodo == "V"):
-        elev_A_tarde = 0
-        elev_A_tarde = +1 
-                
 
-print(respostas)
-conta_elevador()
-print(conta_elevador)
+maiorIndice = elevadoresUtilizados.index(max(elevadoresUtilizados))
+print("O elevador mais utilizado foi o elevador", elevadores[maiorIndice])
+
+turnosUtilizado = [0, 0, 0]
+for resposta in respostas:
+    if resposta[0] == elevadores[maiorIndice]:
+        if resposta[1] == "M":
+            turnosUtilizado[0] += 1
+        if resposta[1] == "V":
+            turnosUtilizado[1] += 1
+        if resposta[1] == "N":
+            turnosUtilizado[2] += 1    
+
+indiceTurno = turnosUtilizado.index(max(turnosUtilizado))
+print("O Elevador ", elevadores[maiorIndice], "foi mais utilizado no truno", )
+buscarMaiorElevador(elevadores)
