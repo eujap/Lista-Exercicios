d893c2468369ce27aa93d572b3387bff4ea374af
DESKTOP-BV2KHFU\ACADEMICO <diego@katecnolog1a.com.br>
2023-03-22 21:15:00 -0400

    inicoi aula dia 22/03

--- exercícios02.py
+++ exercícios02.py
@@ -7,14 +7,14 @@ def validar(msg, validos):
     return valor    
 
 
-def buscarMaiorElevador(opcoes):
-    itens = [0, 0, 0]
+def buscar_maior_quantidade(opcoes):
+    itens = []
     for resposta in respostas:
         for opcao in opcoes:
             itens[opcao.index(opcao)] +=1
    
         
-return opcoes[itens.infr]
+    return opcoes[itens.index(max(itens))]
 
 
 
@@ -24,7 +24,7 @@ respostas = []
 
 while (True):
     elevador = validar("informe o elevador mais utilizado A, B ou C?  ", elevadores)
-    turnos = validar('Informe o turno mais utilizado:( M, V, N)', turnos)
+    turno = validar('Informe o turno mais utilizado:( M, V, N)', turnos)
 
     respostas.append([elevador, turnos])
 
@@ -36,20 +36,8 @@ while (True):
     if continua == "N":
         break
 
+buscar_maior_quantidade(elevadores)
+print("O elevador mais utilizado foi o elevador", buscar_maior_quantidade(elevadores))
+buscar_maior_quantidade(turnos)
+print("O turno mais utilizado foi i turno  ", buscar_maior_quantidade(turnos))
 
-maiorIndice = elevadoresUtilizados.index(max(elevadoresUtilizados))
-print("O elevador mais utilizado foi o elevador", elevadores[maiorIndice])
-
-turnosUtilizado = [0, 0, 0]
-for resposta in respostas:
-    if resposta[0] == elevadores[maiorIndice]:
-        if resposta[1] == "M":
-            turnosUtilizado[0] += 1
-        if resposta[1] == "V":
-            turnosUtilizado[1] += 1
-        if resposta[1] == "N":
-            turnosUtilizado[2] += 1    
-
-indiceTurno = turnosUtilizado.index(max(turnosUtilizado))
-print("O Elevador ", elevadores[maiorIndice], "foi mais utilizado no truno", )
-buscarMaiorElevador(elevadores)
